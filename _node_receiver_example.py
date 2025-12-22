97b875655da392a1a0d384df43287671d09d8e39
DI Martin Novak <martin.novak@htlwienwest.at>
2025-12-22 19:41:57 +0100

    renamed to disabled node: _node.....

---
+++ _node_receiver_example.py
@@ -0,0 +1,18 @@
+# TelemetryBroker for Inter Process Communication for Robtics
+# Client for Receiver Nodes
+# Developed by Martin Novak at 2025/26
+from libs.lib_telemtrybroker import TelemetryBroker
+import time
+
+mb = TelemetryBroker()
+
+
+def cb_getmessage(key, value):
+    print("CALLBACK:", key, value)
+
+
+keys = ["vel_linear_x", "vel_linear_y", "vel_linear_z", "vel_angular_x", "vel_angular_y", "vel_angular_z"]
+mb.setcallback(keys, cb_getmessage)
+
+mb.receiver_loop()
+mb.close()
