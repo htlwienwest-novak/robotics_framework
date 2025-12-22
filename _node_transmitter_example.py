760f035e7d4adaa27a568c80b62ad6c9cba9da4e
DI Martin Novak <martin.novak@htlwienwest.at>
2025-12-22 19:43:31 +0100

    renamed to disabled node: _node.....

---
+++ _node_transmitter_example.py
@@ -0,0 +1,22 @@
+# TelemetryBroker for Inter Process Communication for Robtics
+# Client for Transmitter Nodes
+# Developed by Martin Novak at 2025/26
+import random
+from libs.lib_telemtrybroker import TelemetryBroker
+
+mb = TelemetryBroker() 
+
+vel_dict = {"vel_linear_x":0, "vel_linear_y":0, "vel_linear_z":0, "vel_angular_x":0, "vel_angular_y":0, "vel_angular_z":0}
+
+while True:
+    try:
+        for k, v in vel_dict.items():
+            vel_dict[k] = random.randint(0, 100)
+
+        print(vel_dict)
+        mb.setmulti(vel_dict)
+
+    except KeyboardInterrupt:
+        break
+
+mb.close()
