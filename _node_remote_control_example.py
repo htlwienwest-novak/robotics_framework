72896e88020050a42f1ce2149b4d74c71a9ef039
DI Martin Novak <martin.novak@htlwienwest.at>
2025-12-23 18:23:11 +0100

    manual input added

--- _node_remote_control_example.py
+++ _node_remote_control_example.py
@@ -7,13 +7,24 @@ import os
 
 mb = TelemetryBroker()
 
+vel_dict = {"vel_linear_x":0, "vel_angular_z":0}
+
 while True:
     try:
         #time.sleep(0.5)
         data = mb.getall()
         os.system('cls' if os.name == 'nt' else 'clear')
-        for key, value in data.items():
-            print(key, ":", value)
+        for key, value in sorted(data.items()):
+            print(f"{value:>5}", ":", key)
+        print()
+        # INPUT
+        for key, value in vel_dict.items():
+            invalue = input(str(key) + ":")
+            if invalue == "":
+                invalue = 0
+            vel_dict[key] = invalue
+
+        mb.setmulti(vel_dict)
     except KeyboardInterrupt:
         break
 
