3e76b7b1fc4e6f2478886124349519c48437cfde
DI Martin Novak <martin.novak@htlwienwest.at>
2025-12-25 12:41:20 +0100

    new Node as Master => THE BRAIN

---
+++ node_master.py
@@ -0,0 +1,7 @@
+# TelemetryBroker for Inter Process Communication for Robtics
+# MASTERNODE => THE BRAIN
+# Developed by Martin Novak at 2025/26
+
+from libs.lib_telemtrybroker import TelemetryBroker
+
+mb = TelemetryBroker() 
