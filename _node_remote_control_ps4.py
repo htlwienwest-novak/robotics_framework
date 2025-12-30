8fde0ac9b28856dbf309b4c39edcef5091d6acc5
DI Martin Novak <martin.novak@htlwienwest.at>
2025-12-30 18:40:29 +0100

    changed ps4 controller technology

--- _node_remote_control_ps4.py
+++ _node_remote_control_ps4.py
@@ -2,191 +2,126 @@
 # node for remote ps4 controler 
 # Developed by Martin Novak at 2025/26
 # 
-# Installation:
-# sudo apt install python3-dev python3-pip
-# pip install pyPS4Controller
-#
-# Pairing PS4 Controller with Raspberry Pi:
-#   sudo bluetoothctl
-#   scan on
-#   pair [MAC_ADDRESS]
-#   connect [MAC_ADDRESS]
-#   trust [MAC_ADDRESS]
-#   exit
-
-from pyPS4Controller.controller import Controller
 from libs.lib_telemtrybroker import TelemetryBroker
-import time
+import pygame
+import sys
 
+mb = TelemetryBroker()
 
-class MyController(Controller):
+data_dict = {"vel_linear_x":0, "vel_angular_z":0, "tool_pen":0, "led_blink":0}
 
-    def __init__(self, **kwargs):
-        # Das Interface ist typischerweise /dev/input/js0
-        # Prüfen Sie mit 'ls /dev/input' ob js0 existiert
-        super().__init__(interface="/dev/input/js0", connecting_using_ds4drv=False, **kwargs)
-        self.mb = TelemetryBroker() 
+# Initialisierung
+pygame.init()
+pygame.joystick.init()
 
-    # Beispiel-Handler für Tasten (Buttons)
-    def on_x_press(self):
-        print("Taste X gedrückt")
+# Überprüfen, ob Controller vorhanden sind
+joystick_count = pygame.joystick.get_count()
 
-    def on_x_release(self):
-        print("Taste X losgelassen")
+if joystick_count == 0:
+    print("Kein Controller gefunden.")
+    sys.exit()
+else:
+    # Den ersten Controller auswählen (Index 0)
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    print(f"Controller erkannt: {joystick.get_name()}")
+    print(f"Anzahl Achsen: {joystick.get_numaxes()}")
+    print(f"Anzahl Buttons: {joystick.get_numbuttons()}")
 
-    def on_triangle_press(self):
-        print("on_triangle_press")
+print("-" * 20)
+print("Drücke Tasten auf dem Controller (STRG+C zum Beenden)...")
 
-    def on_triangle_release(self):
-        print("on_triangle_release")
-
-    def on_circle_press(self):
-        print("on_circle_press")
-
-    def on_circle_release(self):
-        print("on_circle_release")
-
-    def on_square_press(self):
-        print("on_square_press")
-
-    def on_square_release(self):
-        print("on_square_release")
-
-    def on_L1_press(self):
-        print("on_L1_press")
-
-    def on_L1_release(self):
-        print("on_L1_release")
-
-    def on_L2_press(self, value):
-        print("on_L2_press: {}".format(value))
-
-    def on_L2_release(self):
-        print("on_L2_release")
-
-    def on_R1_press(self):
-        print("on_R1_press")
-
-    def on_R1_release(self):
-        print("on_R1_release")
-
-    def on_R2_press(self, value):
-        print("on_R2_press: {}".format(value))
-
-    def on_R2_release(self):
-        print("on_R2_release")
-
-    def on_up_arrow_press(self):
-        print("on_up_arrow_press")
-
-    def on_up_down_arrow_release(self):
-        print("on_up_down_arrow_release")
-
-    def on_down_arrow_press(self):
-        print("on_down_arrow_press")
-
-    def on_left_arrow_press(self):
-        print("on_left_arrow_press")
-
-    def on_left_right_arrow_release(self):
-        print("on_left_right_arrow_release")
-
-    def on_right_arrow_press(self):
-        print("on_right_arrow_press")
-
-
-
-    # Beispiel-Handler für Analog-Sticks
-    # Die Werte liegen im Bereich von -32767 bis 32767
-    def on_L3_up(self, value):
-        print("on_L3_up: {}".format(value))
-
-    def on_L3_down(self, value):
-        print("on_L3_down: {}".format(value))
-
-    def on_L3_left(self, value):
-        print("on_L3_left: {}".format(value))
-
-    def on_L3_right(self, value):
-        print("on_L3_right: {}".format(value))
-
-    def on_L3_y_at_rest(self):
-        """L3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_L3_y_at_rest")
-
-    def on_L3_x_at_rest(self):
-        """L3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_L3_x_at_rest")
-
-    def on_L3_press(self):
-        """L3 joystick is clicked. This event is only detected when connecting without ds4drv"""
-        print("on_L3_press")
-
-    def on_L3_release(self):
-        """L3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
-        print("on_L3_release")
-
-    def on_R3_up(self, value):
-        print("on_R3_up: {}".format(value))
-
-    def on_R3_down(self, value):
-        print("on_R3_down: {}".format(value))
-
-    def on_R3_left(self, value):
-        print("on_R3_left: {}".format(value))
-
-    def on_R3_right(self, value):
-        print("on_R3_right: {}".format(value))
-
-    def on_R3_y_at_rest(self):
-        """R3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_R3_y_at_rest")
-
-    def on_R3_x_at_rest(self):
-        """R3 joystick is at rest after the joystick was moved and let go off"""
-        print("on_R3_x_at_rest")
-
-    def on_R3_press(self):
-        """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
-        print("on_R3_press")
-
-    def on_R3_release(self):
-        """R3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
-        print("on_R3_release")
-
-    def on_options_press(self):
-        print("on_options_press")
-
-    def on_options_release(self):
-        print("on_options_release")
-
-    def on_share_press(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_share_press")
-
-    def on_share_release(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_share_release")
-
-    def on_playstation_button_press(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_playstation_button_press")
-
-    def on_playstation_button_release(self):
-        """this event is only detected when connecting without ds4drv"""
-        print("on_playstation_button_release")
-        
-    # Eine vollständige Liste der möglichen Methoden finden Sie in der pyPS4Controller-Dokumentation.
-
-
+# Hauptschleife
 try:
-    controller = MyController()
-    
-    print("Warte auf Controller-Eingaben... (Strg+C zum Beenden)")
-    controller.listen(timeout=60) 
-    
-except FileNotFoundError:
-    print("Controller nicht gefunden unter /dev/input/js0. Stellen Sie sicher, dass er verbunden ist.")
-except Exception as e:
-    print(e)
-          
+    while True:
+        # Event-Handling
+        for event in pygame.event.get():
+            
+            # Button gedrückt
+            if event.type == pygame.JOYBUTTONDOWN:
+                #print(f"Button gedrückt: ID {event.button}")
+                if event.button == 0:   # X-Button
+                    print("X-Button gedrückt")
+                    if data_dict["tool_pen"] == 0:
+                        data_dict["tool_pen"] = 1
+                    else:
+                        data_dict["tool_pen"] = 0
+
+                elif event.button == 1: # Circle-Button
+                    print("Circle-Button gedrückt")      
+                    data_dict["led_blink"] = 1
+                elif event.button == 2: # Square-Button
+                    print("Square-Button gedrückt")
+                elif event.button == 3: # Triangle-Button
+                    print("Triangle-Button gedrückt")
+                elif event.button == 4: # Share-Button
+                    print("Share-Button gedrückt")
+                elif event.button == 5: # Start-Button
+                    print("Start-Button gedrückt")
+                elif event.button == 6: # Options-Button
+                    print("Options-Button gedrückt")
+                elif event.button == 7: # LeftStick-Button
+                    print("LeftStick-Button gedrückt")
+                elif event.button == 8: # RightStick-Button
+                    print("RightStick-Button gedrückt")
+                elif event.button == 9: # L1-Button
+                    print("L1-Button gedrückt")
+                elif event.button == 10: # R1-Button
+                    print("R1-Button gedrückt")
+                elif event.button == 11: # Cross-Top-Button
+                    print("Cross-Top-Button gedrückt")
+                elif event.button == 12: # Cross-Bottom-Button
+                    print("Cross-Bottom-Button gedrückt")
+                elif event.button == 13: # Cross-Left-Button
+                    print("Cross-Left-Button gedrückt")
+                elif event.button == 14: # Cross-Right-Button
+                    print("Cross-Right-Button gedrückt")
+            
+            # Button losgelassen
+            elif event.type == pygame.JOYBUTTONUP:
+                pass
+            
+            # Analoge Achsen (Sticks & Trigger L2/R2)
+            elif event.type == pygame.JOYAXISMOTION:
+                # Wir filtern sehr kleine Bewegungen (Deadzone), um Spam zu vermeiden
+                if abs(event.value) > 0.1: 
+                    #print(f"Achse {event.axis} bewegt: {int((event.value*100+100)/2):.2f}")
+                    if event.axis == 0:
+                        val = int(event.value*10)*10
+                        if (val<20 and val>0) or (val>-20 and val<0):
+                            val=0
+                        print("Left-Stick-X bewegt:", val)
+                        data_dict["vel_angular_z"] = val
+                    elif event.axis == 1:
+                        print("Left-Stick-Y bewegt:", int(event.value*100))
+                    elif event.axis == 2:
+                        print("Right-Stick-X bewegt:", int(event.value*100))
+                    elif event.axis == 3:
+                        print("Right-Stick-Y bewegt:", int(event.value*100))
+                    elif event.axis == 4:
+                        val = -1*int((event.value*100+100)/2)
+                        print("L2-Trigger bewegt:", val)
+                        data_dict["vel_linear_x"] = val
+                    elif event.axis == 5:
+                        val = int((event.value*100+100)/2)
+                        print("R2-Trigger bewegt:", val)
+                        data_dict["vel_linear_x"] = val
+            
+            # D-Pad (Steuerkreuz)
+            elif event.type == pygame.JOYHATMOTION:
+                # event.value ist ein Tuple, z.B. (0, 1) für Oben
+                print(f"D-Pad (Hat {event.hat}) bewegt: {event.value}")
+                
+            # Quit Event (falls ein Fenster offen wäre)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            mb.setmulti(data_dict)
+
+        # Kurze Pause, um CPU-Last zu verringern (Clock tick wäre in einem Spiel besser)
+        pygame.time.wait(10)
+
+except KeyboardInterrupt:
+    print("\nProgramm beendet.")
+    pygame.quit()
