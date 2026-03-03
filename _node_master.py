a7a4bf283be6fbaea0d4d0fa9a2b9fe85c279d17
Martin Novak <martin.novak@htlwienwest.at>
2026-03-03 15:13:53 +0100

    cycle

--- _node_master.py
+++ _node_master.py
@@ -5,3 +5,17 @@
 from libs.lib_telemtrybroker import TelemetryBroker
 
 mb = TelemetryBroker() 
+
+# One cycle from tile to tile:
+# 1. analyze_walls => wall_state = {"0":True, "90":True, "180":True, "270":True}
+# 2. do_mapping
+# 3. decide_drive_direction => drive_direction
+# 4. analyze_floor => floor_color
+# 5. do_floor_action => e.g. blue=>wait 5s
+# 6. analyze_victims => victim_color, victim_letter
+# 7. do_victim_action => set rescue kits, blink led
+# 8. drive_rotation => until angle
+# 9. drive_next => until next tile
+# when target tile arrived, then GOTO 1
+
+mb.close()
