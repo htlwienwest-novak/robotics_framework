0c45eb9866f2f5403f6342fff3999885da20fc03
DI Martin Novak <martin.novak@htlwienwest.at>
2026-03-06 12:08:09 +0100

    easy input with number selection for keys

--- _node_remote_control_terminal.py
+++ _node_remote_control_terminal.py
@@ -8,22 +8,31 @@ import os
 mb = TelemetryBroker()
 
 data_dict = {}
+num = 0
+key_list = []
 
 while True:
     try:
         #time.sleep(0.5)
         data = mb.getall()
         os.system('cls' if os.name == 'nt' else 'clear')
+        key_list = []
+        num = 0
         for key, value in sorted(data.items()):
-            print(f"{key}"+" : "+value+"\n")
+            key_list.append(key)
+            print(str(num) + " :: "+ f"{key}" + " = " + value)
+            num += 1
         print()
 
         # INPUT
-        input_key = input("key:")
-        input_val = input("val:")
+        input_key = input("key or num: ")
+        input_val = input("value: ")
 
         if input_key == "" or input_val == "":
             continue
+
+        if input_key.isdigit():
+            input_key = key_list[int(input_key)]
 
         data_dict[input_key] = input_val
         mb.setmulti(data_dict)
