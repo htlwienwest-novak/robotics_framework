139ed7b4124dd3bf0387f01fb73e459462914104
DI Martin Novak <martin.novak@htlwienwest.at>
2026-03-17 07:50:47 +0100

    node http server can get post and get reuqests

--- _node_http_server.py
+++ _node_http_server.py
@@ -9,21 +9,27 @@ mb = TelemetryBroker()
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def handle_json():
-    # Prüfen, ob die Daten im JSON-Format vorliegen
-    if request.is_json:
-        # Den JSON-String automatisch in ein Python-Dictionary umwandeln
-        data = request.get_json()
-        print(f"Empfangene Daten: {data}")
+    if request.method == 'POST':
+        # Prüfen, ob die Daten im JSON-Format vorliegen
+        if request.is_json:
+            # Den JSON-String automatisch in ein Python-Dictionary umwandeln
+            data = request.get_json()
+            print(f"Empfangene Daten: {data}")
 
+            for key, value in data.items():
+                mb.set(key, value)  # Setze die Werte im TelemetryBroker
+
+            # Eine Antwort zurückgeben
+            return "ok", 200
+        else:
+            return jsonify({"fehler": "Anfrage muss JSON sein"}), 400
+    elif request.method == 'GET':
+        data = request.args.to_dict()
         for key, value in data.items():
             mb.set(key, value)  # Setze die Werte im TelemetryBroker
-
-        # Eine Antwort zurückgeben
         return "ok", 200
-    else:
-        return jsonify({"fehler": "Anfrage muss JSON sein"}), 400
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
