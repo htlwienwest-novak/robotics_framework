f4cc6bc437fd19a75cb12e35dc1b696d447cb7ae
DI Martin Novak <martin.novak@htlwienwest.at>
2026-03-28 12:07:28 +0100

    changed to permanent active nodes

---
+++ node_remote_control_terminal.py
@@ -0,0 +1,46 @@
+# TelemetryBroker for Inter Process Communication for Robtics
+# Node as Remote Control via Console
+# Developed by Martin Novak at 2025/26
+from libs.lib_telemtrybroker import TelemetryBroker
+import time
+import os
+
+mb = TelemetryBroker()
+
+data_dict = {}
+num = 0
+key_list = []
+
+while True:
+    try:
+        #time.sleep(0.5)
+        data = mb.getall()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        key_list = []
+        num = 0
+        
+        for key, value in sorted(data.items()):
+            key_list.append(key)
+            print(str(num) + " :: "+ f"{key}" + " = " + f"{value}")
+            num += 1
+        print()
+        print("ENTER => REFRESH ALL DATA")
+
+        # INPUT
+        input_key = input("key or num: ")
+
+        if input_key == "":
+            continue
+
+        input_val = input("value: ")
+
+        if input_key.isdigit():
+            input_key = key_list[int(input_key)]
+
+        data_dict[input_key] = input_val
+        mb.setmulti(data_dict)
+
+    except KeyboardInterrupt:
+        break
+
+mb.close()
---
+++ node_remote_display_terminal.py
@@ -0,0 +1,34 @@
+# TelemetryBroker for Inter Process Communication for Robtics
+# Node as Remote Control via Console
+# Developed by Martin Novak at 2025/26
+from libs.lib_telemtrybroker import TelemetryBroker
+import time
+import os
+
+mb = TelemetryBroker()
+
+vel_dict = {"vel_linear_x":0, "vel_angular_z":0, "tool_pen":0, "led_blink":0}
+
+
+CURSOR_UP_LEFT = "\033[H"  # Springt nach ganz oben links (Home)
+HIDE_CURSOR = "\033[?25l"  # Versteckt den blinkenden Cursor (optional)
+SHOW_CURSOR = "\033[?25h"  # Zeigt ihn wieder an
+
+print(HIDE_CURSOR, end="") # Cursor verstecken für besseren Look
+
+while True:
+    try:
+        #time.sleep(0.1)
+        data = mb.getall()
+        output = ""
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for key, value in sorted(data.items()):
+            output = output + f"{key}" + " : " + f"{value}" +"\n"
+        
+        print(f"{CURSOR_UP_LEFT}{output}", end="\r", flush=True)
+
+    except KeyboardInterrupt:
+        print(SHOW_CURSOR)
+        break
+
+mb.close()
