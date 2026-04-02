87d2531f4ca439d8788fb4b56de6f53a8056addd
DI Martin Novak <martin.novak@htlwienwest.at>
2026-04-02 18:56:32 +0200

    show ip adress for eth0

--- node_system_operations.py
+++ node_system_operations.py
@@ -3,13 +3,32 @@
 # Developed by Martin Novak at 2025/26
 from libs.lib_telemtrybroker import TelemetryBroker
 import time
+import subprocess
 import os
 
 mb = TelemetryBroker()
 
 datadict = {"reboot":0, "shutdown":0}
-mb.setmulti(datadict)
 
+
+def get_ip_address(interface='eth0'):
+    try:
+        # Führt den Befehl 'ip -4 addr show eth0' aus
+        output = subprocess.check_output(['ip', '-4', 'addr', 'show', interface]).decode('utf-8')
+        
+        # Sucht nach der Zeile mit 'inet' und extrahiert die IP
+        for line in output.split('\t'):
+            if 'inet ' in line:
+                return line.split()[1].split('/')[0]
+    except Exception as e:
+        return ""
+
+ip_eth0 = get_ip_address("eth0")
+if ip_eth0 != "":
+    datadict["ip"] = ip_eth0
+    print(f"Ethernet IP: {ip_eth0}")    
+
+mb.setmulti(datadict)
 
 while True:
     try:
@@ -27,5 +46,3 @@ while True:
         break
 
 mb.close()
-
-
